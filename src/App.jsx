5e4e2449c66ac6615659008e6f8b0a81193a3914
Dai Cat <phandaicat12032002@gmail.com>
2023-06-15 22:54:26 +0700

    50 . useState - two_way binding - input

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import './App.css';
 
 function App() {
 
-  const gifts = [
-    'CPU',
-    'RAM',
-    "ROM"
-  ]
+  const [username , setUsername] = useState('');
+  const [email , setEmail] = useState('')
 
-  const [gift , setGift] = useState()
 
-  
-  const handleGift = () =>{
-    setGift(gifts[Math.floor(Math.random()*3)])
+  const handleSubmit = () => {
+    console.log({
+      username : username,
+      email : email,
+    }); 
   }
 
+  
   return (
     <div className="App" style={{padding:50}}>
-      <h1>{gift || "Chua co phan thuong"}</h1>
-      <button onClick={handleGift}>Lay thuong</button>
+      <label >User name : </label>
+      <input  
+        onChange={e => setUsername(e.target.value)} 
+        value={username}
+      />
+
+      <br />
+      <label >Email : </label>
+      <input  
+        onChange={e => setEmail(e.target.value)} 
+        value={email}/>
+
+      <br />
+      <button onClick={handleSubmit}>Gui</button>
     </div>
   );
 }
