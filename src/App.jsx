523806f72adfeb5c91dae5677bdc6dce0b0e011a
Dai Cat <phandaicat12032002@gmail.com>
2023-06-15 23:16:15 +0700

    50 . useState-two_way binding - radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,50 @@
 import { useState } from 'react';
 import './App.css';
 
+// response from API
+const courses = [
+  {
+    id: 1,
+    name : 'HTML , CSS'
+  },
+  {
+    id: 2,
+    name: 'Javasript'
+  },
+  {
+    id: 3,
+    name: 'ReactJS'
+  }
+]
+
 function App() {
 
-  const [username , setUsername] = useState('');
-  const [email , setEmail] = useState('')
+  const [checked , setChecked] = useState()
 
+  console.log((checked));
+  
 
   const handleSubmit = () => {
-    console.log({
-      username : username,
-      email : email,
-    }); 
+    console.log({id : checked});
   }
 
   
   return (
     <div className="App" style={{padding:50}}>
-      <label >User name : </label>
-      <input  
-        onChange={e => setUsername(e.target.value)} 
-        value={username}
-      />
-
-      <br />
-      <label >Email : </label>
-      <input  
-        onChange={e => setEmail(e.target.value)} 
-        value={email}/>
-
-      <br />
-      <button onClick={handleSubmit}>Gui</button>
+      {
+        courses.map((course ) => (
+          <div key={course.id}>
+            <input 
+              type="radio" 
+              checked={checked === course.id}
+              onChange={() => setChecked(course.id)}
+            /> 
+            {course.name}
+          </div>
+         ) )
+      }
+     
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
