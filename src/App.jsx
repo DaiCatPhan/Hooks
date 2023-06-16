57aac0fdf86d2a78aa791b751857edb042a97afd
Dai Cat <phandaicat12032002@gmail.com>
2023-06-16 11:06:10 +0700

    52. Mounted & Unmounted?

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
-import { useRef, useState } from 'react';
-import './App.css';
-
-
+import { useState } from 'react';
+import Content from './Content'
 
 function App() {
 
-  const [job, setJob] = useState('')
-  const [jobs, setJobs] = useState(() => {
-    const storageJobs =JSON.parse( localStorage.getItem('jobs'))
-    return storageJobs
-  }) 
-  const inputRef = useRef()
-
-  console.log(job);
-
-  const handleOnChange = (e) => {
-    setJob(e.target.value)
-  }
-
+  const [on , setOn] = useState(false)
 
   const handleSubmit = () => {
-    setJobs(prev => {
-      const newJobs = [...prev , job];
-      const jsonJobs = JSON.stringify(newJobs)
-      localStorage.setItem('jobs',jsonJobs)
-
-      return newJobs;
-    })
-    setJob('')
-    inputRef.current.focus()
+    setOn(!on);
   }
-
-
   
   return (
     <div className="App" style={{padding:50}}>
-      <input 
-        type="text" 
-        value={job}
-        ref={inputRef}
-        onChange={handleOnChange}
-      />
-     
-      <button onClick={handleSubmit}>Add</button>
-      
-      <ul>
-        {
-          jobs.map((job , index) => (
-            <li key={index}>{job}</li>
-          ))
-        }
-      </ul>
+      <button onClick={handleSubmit}> Submit</button>
+      {on && <Content />}
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,8 @@
+
+function Content() {
+    return ( 
+        <h1>Hi anh em F8</h1>
+     );
+}
+
+export default Content;
