a7e81b8d55cdef60e7dc81f74fb6893f5ff5f759
Dai Cat <phandaicat12032002@gmail.com>
2023-06-19 19:12:41 +0700

    59. useEffect with timer functions

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,87 +1,25 @@
 import {  useEffect, useState } from "react";
 
-const tabs = ['posts' , 'comments' , 'albums'];
 
 function Content() {
 
-    // const [title , setTitle] = useState('')
-    const [posts , setPosts] = useState([])
-    const [type , setType] = useState('posts')
-    const [showTop , setShowTop] = useState(false)
 
-    console.log(type);
+    const [countdown,setCountdown] = useState(180)
 
     useEffect(() => {
-       fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res => res.json())
-        .then(posts => {
-           setPosts(posts)
-        })
-    },[type])
-
-    useEffect(() => {
-
-        const handleScroll = () => {
-            if(window.scrollY > 200) {
-                setShowTop(true)
-            }else{
-                setShowTop(false)
-            }
-
-        }
-        window.addEventListener('scroll' , handleScroll)
+        const timerId = setInterval(() => {
+            setCountdown(pre => pre -1)
+        },1000)
 
 
         return () => {
-            window.removeEventListener('scroll' , handleScroll)
+            clearInterval(timerId)
         }
-
     },[])
 
     return ( 
         <>
-            <div>
-                {
-                    tabs.map(tab => (
-                        <button 
-                            key={tab} 
-                            style={type === tab ? {
-                                color: '#fff',
-                                backgroundColor: '#333'
-                            } : {}}
-                            onClick={() => setType(tab)}
-                        >
-                            {tab}
-                        </button>
-                    ))
-                }
-            </div>
-
-            {/* <input type="text" 
-                onChange={e => setTitle(e.target.value)}
-                value={title}
-            /> */}
-            <ul>
-                {
-                   posts.map(post => (
-                    <li key={post.id}>{post.title || post.name  }</li>
-                   ))
-                }
-            </ul>
-
-            {
-                showTop && (
-                    <button
-                        style={{
-                            position: 'fixed',
-                            right: 20,
-                            bottom: 20,
-                        }}
-                    >
-                        Top
-                    </button>
-                )
-            }
+            <h1>{countdown}</h1>
         </>
      );
 }
