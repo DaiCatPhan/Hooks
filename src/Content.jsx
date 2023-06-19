ffdda5aca8059ef2a38fc4e5988d5ba372892b73
Dai Cat <phandaicat12032002@gmail.com>
2023-06-19 22:39:43 +0700

    61. useEffect with fake Chat App

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,36 +1,62 @@
 import {  useEffect, useState } from "react";
 
+const lessons = [
+    {
+        id: 1,
+        name: 'ReactJS la gi ? Tai sao phai hoc ReactJs'
+    },
+    {
+        id: 2,
+        name: 'SPA / MPA la gi'
+    },
+    {
+        id: 3,
+        name: 'Arrow function'
+    }
+]
+
 
 function Content() {
-    const [avatar , setAvatar] = useState()
+    const [lessonId , setLessonId] = useState(1)
 
     useEffect(() => {
 
-        // Clean up
-        return () => {
-            avatar && URL.revokeObjectURL(avatar.preview)
+        const handleComment = (e) => {
+            console.log(e.detail);
         }
-    },[avatar])
 
-    const handlePriviewAvatar = (e) => {
-        const file = e.target.files[0];
-        file.preview = URL.createObjectURL(file);
-        setAvatar(file)
-    }
+        window.addEventListener(`lesson-${lessonId}` , handleComment)
+
+        return () => {
+            window.removeEventListener(`lesson-${lessonId}` , handleComment)
+        }
+    },[lessonId])
     
+    const handleClick = (id) => {
+        setLessonId(id)
+    }
 
     return ( 
+
+
         <>
-            <input type="file" 
-                onChange={handlePriviewAvatar}
-
-            />
-
-            {
-                avatar && (
-                    <img src={avatar.preview} alt="Dai Cat"  width="50%"/>
-                )
-            }
+            <ul>
+                {
+                    lessons.map(lesson => (
+                        <li
+                            key={lesson.id}
+                            style={
+                                lessonId === lesson.id ? {
+                                    color: 'red'
+                                } : {}
+                            }
+                            onClick={() => handleClick(lesson.id)}
+                        >
+                            {lesson.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </>
      );
 }
