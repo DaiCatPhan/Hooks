fe6da92e16dfa448a815be92e061eb42851028e0
Dai Cat <phandaicat12032002@gmail.com>
2023-06-20 21:52:30 +0700

    68. useCallback hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import {  useCallback, useState } from "react";
 import Content from "./Content";
 
 
@@ -6,17 +6,31 @@ function App() {
 
   const [count , setCount] = useState(0)
 
-  const handleIncrease = () => {
-      setCount(prev => prev + 1)
-  }
+  const handleIncrease = useCallback(() => {
+    setCount(prev => prev + 1)
+  },[])
   
   return (
     <div className="App" style={{padding:50}}>
-      <Content/>
+      <Content onIncrease={handleIncrease}/>
       <h1 >{count}</h1>
-      <button onClick={handleIncrease}>Click me !</button>
     </div>
   );
 }
 
 export default App;
+
+// useCallback : tránh lặp lại những cái hàm  không cần thiết khi truyền props cho component con
+// Phải sài react memo thì mới dùng useCallback
+
+// lần 1 thì hàm handleIncrease được tạo ra và tham chiếu vào ô nhớ 1 
+// khi bấm button thì setCount , chạy lại từ đầu và tạo ra function handleIncrease lại thì tham chiếu vào ô nhớ mới là 2
+// bên Content thằng memo nó đón đâu và so sánh  props là onIncrease bằng toán tử === 
+// thì 2 thằng có tham chiếu khác nhau nên nó tưởng là props hay đổi nên render lại 
+
+
+// Khi sài thằng useCallback(callback , []) khi mà tạo hàm thì nó tham chiếu vào ô nhớ 1
+// Thì nó trả về tham chiếu 1 đó cho useCallback lưu ra ngoài component luôn , tự nó lưu
+// sau đó nó return lại cái tham chiếu đó cho biến handleIncrease 
+// mà cái [deps] trống thì nó trả về tham chiếu trước đó 
+// Nên tham chiếu được thêm vào không thay đổi thì k render lại
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,18 +1,18 @@
 import { memo } from "react";
 
-function Content() {
+function Content({onIncrease}) {
     console.log('re-render');
     
     
     return ( 
         <>
            <h2>Hello anh em F8</h2>
+            <button onClick={onIncrease}>Click me !</button>
         </>
      );
 }
 
 export default memo(Content);
 
-// memo check các props của component Content xem nó có thay đỏi hay không
-// Nếu có thay đổi thì nó render lại component Content
-// Nếu k thay đổi thì nó khong render lại Content
+// useCallback : tránh tạo ra những cái hàm mới không cần thiết trong component của bạn
+// Phải sài react memo thì mới dùng useCallback
