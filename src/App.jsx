0fa4f0053c55d729fce062596ba288b75ebb6d9b
Dai Cat <phandaicat12032002@gmail.com>
2023-06-20 22:31:58 +0700

    70. useMemo hook 11:06

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,71 @@
-import {  useCallback, useState } from "react";
-import Content from "./Content";
+import { type } from "@testing-library/user-event/dist/type";
+import { useMemo, useRef } from "react";
+import {  useState } from "react";
 
 
 function App() {
 
-  const [count , setCount] = useState(0)
+  const [name , setName] = useState('')
+  const [price , setPrice] = useState('')
+  const [products , setProducts] = useState([])
 
-  const handleIncrease = useCallback(() => {
-    setCount(prev => prev + 1)
-  },[])
+  const nameRef = useRef()
+
   
+
+
+
+  const handleSubmit = () => {
+    setProducts([...products , {
+      name,
+      price : +price
+    }])
+
+    setName('')
+    setPrice('')
+    nameRef.current.focus()
+  }
+
+  const total = useMemo(() => {
+    const result = products.reduce((result , prod) => {
+      console.log('hi');
+      return result + prod.price
+    },0)  
+    return result 
+  } , [products])
+
   return (
     <div className="App" style={{padding:50}}>
-      <Content onIncrease={handleIncrease}/>
-      <h1 >{count}</h1>
+      <input type="text" 
+        ref={nameRef}
+        value={name}
+        placeholder="Enter name..."
+        onChange={e=>setName(e.target.value)}
+      />
+
+      <br />
+
+      <input type="text" 
+        value={price}
+        placeholder="Enter price..."
+        onChange={e=>setPrice(e.target.value)}
+      />
+
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+
+      <br />
+
+      Total: {total}
+      {
+        products.map((product , index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))
+      }
+
     </div>
   );
 }
 
 export default App;
 
-// useCallback : tránh lặp lại những cái hàm  không cần thiết khi truyền props cho component con
-// Phải sài react memo thì mới dùng useCallback
-
-// lần 1 thì hàm handleIncrease được tạo ra và tham chiếu vào ô nhớ 1 
-// khi bấm button thì setCount , chạy lại từ đầu và tạo ra function handleIncrease lại thì tham chiếu vào ô nhớ mới là 2
-// bên Content thằng memo nó đón đâu và so sánh  props là onIncrease bằng toán tử === 
-// thì 2 thằng có tham chiếu khác nhau nên nó tưởng là props hay đổi nên render lại 
-
-
-// Khi sài thằng useCallback(callback , []) khi mà tạo hàm thì nó tham chiếu vào ô nhớ 1
-// Thì nó trả về tham chiếu 1 đó cho useCallback lưu ra ngoài component luôn , tự nó lưu
-// sau đó nó return lại cái tham chiếu đó cho biến handleIncrease 
-// mà cái [deps] trống thì nó trả về tham chiếu trước đó 
-// Nên tham chiếu được thêm vào không thay đổi thì k render lại
